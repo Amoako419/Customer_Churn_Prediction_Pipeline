cb831d59ae0badeb7ec0fdd520903f9963137859
Amoako419 <ghheskey@gmail.com>
2025-05-21 09:12:47 +0000

    Update MLflow client tracking URI from port 8088 to 5000

--- scripts/ml_app.py
+++ scripts/ml_app.py
@@ -2,7 +2,7 @@ from mlflow import MlflowClient
 from pprint import pprint
 from sklearn.ensemble import RandomForestRegressor
 
-client = MlflowClient(tracking_uri="http://127.0.0.1:8088")
+client = MlflowClient(tracking_uri="http://127.0.0.1:5000")
 
 all_experiments = client.search_experiments()
 
